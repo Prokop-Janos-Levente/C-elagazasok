f62007df4d885f2e6dc37cba22c17350bb1ab750
Levi 91 <prokopj@kkszki.hu>
2025-09-18 08:20:50 +0200

    c# is created.

---
+++ elagazasok/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elagazasok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy szémot: ");
+            int szam = 
+        }
+    }
+}
